d0dddb7e978afe70231b2347ff671f1a33d48f99
SidZhoop <siddharth@zhoop.in>
2023-08-18 16:07:28 +0530

    footer completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div>©2023 divya_. All rights reserved.</div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.icons}
+          alt="divya"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.icons}
+          alt="divya"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icons}
+          alt="divya"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.icons}
+          alt="divya"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
